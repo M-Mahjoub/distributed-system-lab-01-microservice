349b1c3c481251d01b0ddd38f29e8f5b7fa96b2e
Majid <Majid@DESKTOP-CIU1PSJ>
2026-07-03 22:34:51 +0330

    create product

--- src/ProductService/Domain/Product.cs
+++ src/ProductService/Domain/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,41 @@ namespace Domain
 {
     public class Product
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public string SKU { get; set; }
-        public decimal Price { get; set; }
-        public string Currency { get; set; }
+        public Product(string name, string sku, string currency)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
 
-        public Product() { }
+            if (string.IsNullOrEmpty(sku))
+                throw new ArgumentNullException("sku");
+
+            if (string.IsNullOrEmpty(currency))
+                throw new ArgumentNullException("currency");
+
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+            this.SKU = sku;
+            this.Currency = currency;
+            this.Status = 0;
+            this.CreateAt = DateTime.UtcNow;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; private set; }
+        public ProductStatus Status { get; private set; }
+        public string Category { get; private set; }
+        public string SKU { get; private set; }
+        public decimal Price { get; private set; }
+        public string Currency { get; private set; }
+        public DateTime CreateAt { get; private set; }
+        public string Description { get; private set; }
+
+        public void Rename(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+
+            this.Name = name;
+        }
     }
 }
---
+++ src/tests/UnitTests/Domain/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Domain;
+
+namespace UnitTests.Domain
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Should_Create_Product_When_Data_Is_Valid()
+        {
+            var produt = new Product("TV", "T100", "USD");
+
+            if (produt != null)
+                Assert.Pass();
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Should_Throw_When_Name_Is_Empty()
+        {
+            try
+            {
+                var produt = new Product("", "T100", "USD");
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException)
+                    Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Should_Throw_When_SKU_Is_Empty()
+        {
+            try
+            {
+                var produt = new Product("TV", "", "USD");
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException)
+                    Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Should_Throw_When_Currency_Is_Empty()
+        {
+            try
+            {
+                var produt = new Product("TV", "", "");
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException)
+                    Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+    }
+}
